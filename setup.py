c5ed4ad3a2fb18c824a2f122f732f4df1f74af25
Chris Rossi <chris@archimedeanco.com>
2009-12-24 18:38:01 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1dev'
+
+INSTALL_REQUIRES=[]
+
+setup(name='edwin',
+      version=version,
+      description="",
+      long_description='',
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=INSTALL_REQUIRES,
+      tests_require=INSTALL_REQUIRES + ['nose', 'coverage'],
+      test_suite='edwin',
+      entry_points={
+          }
+      )
