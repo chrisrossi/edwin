9d375b4450af93c01afb1f028129f2947cf67f2d
Chris Rossi <chris@archimedeanco.com>
2009-12-28 15:54:39 -0500

    Create album model with title property.

---
+++ src/edwin/edwin/models/album.py
@@ -0,0 +1,32 @@
+from __future__ import with_statement
+import os
+
+_marker = object()
+
+class _FSProperty(object):
+    value = _marker
+
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, album, cls=None):
+        if self.value is _marker:
+            fpath = os.path.join(album.path, '.%s' % self.name)
+            if os.path.exists(fpath):
+                self.value = open(fpath).read()
+            else:
+                self.value = None
+        return self.value
+
+    def __set__(self, album, value):
+        fpath = os.path.join(album.path, '.%s' % self.name)
+        with open(fpath, 'w') as f:
+            f.write(value)
+        self.value = value
+
+class Album(object):
+    title = _FSProperty('title')
+
+    def __init__(self, path):
+        self.path = path
+
---
+++ src/edwin/edwin/models/tests/test_album.py
@@ -0,0 +1,23 @@
+import unittest
+
+class TestAlbum(unittest.TestCase):
+    def setUp(self):
+        import tempfile
+        self.path = tempfile.mkdtemp('_test', 'edwin_')
+
+    def tearDown(self):
+        import shutil
+        shutil.rmtree(self.path)
+
+    def _make_one(self, test_photos=0):
+        from edwin.models.album import Album
+        return Album(self.path)
+
+    def test_title(self):
+        album = self._make_one()
+        self.assertEqual(album.title, None)
+        album.title = "Test Foo"
+
+        album = self._make_one()
+        self.assertEqual(album.title, "Test Foo")
+
